ce4fce3fb7a859f6863ca2a830752f729c7aac33
Jensen Judkins <thesmelliestman@gmail.com>
2018-12-14 09:03:26 -0700

    InternetViewCOntroller done

--- InternetApp/Controller/InternetDetailViewController.swift
+++ InternetApp/Controller/InternetDetailViewController.swift
@@ -7,13 +7,72 @@
 //
 
 import UIKit
+import WebKit
 
-class InternetDetailViewController: UIViewController {
+class InternetDetailViewController: UIViewController
+{
 
+    @IBOutlet weak var webViewer: WKWebView!
+    @IBOutlet weak var screenTitle: UILabel!
+    var detailTitle : String?
+    {
+        didSet
+        {
+            updateView()
+        }
+    }
+    
+    var detailAddress : String?
+    {
+        didSet
+        {
+            updateView()
+        }
+    }
+    
+    private func updateView() -> Void
+    {
+        if(detailTitle != nil && screenTitle != nil && webViewer != nil)
+        {
+            if(detailTitle?.range(of: "Definitions", options: .caseInsensitive) != nil)
+            {
+                
+                loadPDF()
+            }
+            else
+            {
+                if(detailAddress != nil)
+                {
+                    loadURL(webAddress: detailAddress!)
+                }
+            }
+        screenTitle?.text = detailTitle
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateView()
 
         // Do any additional setup after loading the view.
+    }
+    
+    private func loadURL(webAddress: String) -> Void
+    {
+        let currentURL = URL(string: webAddress)
+        let currentWebRequest = URLRequest(url: currentURL!)
+        webViewer.load(currentWebRequest)
+    }
+    
+    private func loadPDF() -> Void
+    {
+        if let contentPDF = Bundle.main.url(forResource: "Your PDF Name HERE", withExtension: "pdf", subdirectory: nil, localization: nil)
+        {
+            let requestedPDF = NSURLRequest(url: contentPDF)
+            webViewer.load(requestedPDF as URLRequest)
+        }
     }
     
 
